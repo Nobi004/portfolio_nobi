1472cb611ef6d85f97caa8d01326004ef358b2c0
Mahmudun Nobi <148976917+Nobi004@users.noreply.github.com>
2024-04-13 04:45:02 +0600

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ with col1:
 with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
-    st.write("A B O U T M E")
+    st.subheader("A B O U T M E")
     st.write("""An enthusiastic AI and ML enthusiast and electrical and
 electronic engineer, dedicated to using engineering skills to
 solve practical problems. Committed to staying on the cutting
